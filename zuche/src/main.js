526bd0a7e1c1daed5ce6edc9217e0cab98aa9ca9
long-su <1243454403@qq.com>
2019-08-25 20:00:02 +0800

    whl

---
+++ zuche/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+
+import './../static/css/common.css'
+import {obj} from'./../static/js/utils'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  mounted(){
+    console.log(obj)
+     obj.setRem()
+     window.onresize=obj.setRem
+  },
+  beforeCreate:function(){
+    //  this.$router.push('/home')
+  }
+})
